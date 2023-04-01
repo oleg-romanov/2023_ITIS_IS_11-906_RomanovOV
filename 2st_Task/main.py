416b72c00028e62caa71940162e62b8b3b3a9d80
Oleg Romanov <olegromanov.3003@gmail.com>
2023-04-01 10:28:41 +0300

    update main.py

--- 2st_Task/main.py
+++ 2st_Task/main.py
@@ -1,19 +1,25 @@
 # Библиотека позволяющая лемматизировать (взять начальную форму слова) токены (слова)
 import pymorphy3
-import glob
 import io
 import os
-import shutil
 
 morph = pymorphy3.MorphAnalyzer()
 
-functors_pos = {'INTJ', 'PRCL', 'CONJ', 'PREP'}
+# INTJ - (междометья - эмоц. состояние говорящего)
+# PRCL - (Частица)
+# CONJ - (союзы)
+# PREP - (предлог)
+functors_pos = {
+    'INTJ', 
+    'PRCL', 
+    'CONJ', 
+    'PREP'
+    }
 path_of_files = "texts/"
 
-def pos(word):
-    "Return a likely part of speech for the *word*."""
+def part_of_speech(word):
     morth = pymorphy3.MorphAnalyzer()
-    # Получаем часть речи слова
+    # Получаем часть речи слова сущ, глаг и тд POS - Part Of Speech
     return morth.parse(word)[0].tag.POS
 
 
@@ -26,13 +32,13 @@ def lemmatize(words, tokenized_map):
     for word in words:
         if pos(word) not in functors_pos:
             p = morph.parse(word)[0].normal_form
-            arr = tokenized_map.get(p)
-            if arr is None:
-                new_arr = set()
-                new_arr.add(word)
-                tokenized_map[p] = new_arr
+            array = tokenized_map.get(p)
+            if array is None:
+                new_array = set()
+                new_array.add(word)
+                tokenized_map[p] = new_array
             else:
-                arr.add(word)
+                array.add(word)
 
 
 def get_clear_words(text):
@@ -45,13 +51,13 @@ def get_clear_words(text):
 
 
 def read_text_from_file(file):
-    with io.open(file, mode='r', encoding="utf-8") as f:
-        return f.read()
+    with io.open(file, mode='r', encoding="utf-8") as file:
+        return file.read()
 
 
 def get_files_from_path(path):
-    arr = os.listdir(path)
-    return arr
+    array = os.listdir(path)
+    return array
 
 files = get_files_from_path(path_of_files)
 
@@ -62,25 +68,24 @@ if not os.path.exists('2st_Task/lemmas'):
     os.mkdir('2st_Task/lemmas')
 
 
-
-for f in files:
-    print(f)
-    if f > '0':
-        text = read_text_from_file(path_of_files + f)
+for file in files:
+    print(file)
+    if file > '0':
+        text = read_text_from_file(path_of_files + file)
         words = get_clear_words(text)
         tokenized_map = {}
         lemmatize(words, tokenized_map)
 
         all_tokens = tokenized_map.values()
-        with io.open('2st_Task/tokens/tokens_' + str(f), 'w', encoding="utf-8") as ff:
+        with io.open('2st_Task/tokens/tokens_' + str(file), 'w', encoding="utf-8") as new_file:
             all_tokens =  [item for sublist in all_tokens for item in sublist]
             all_tokens.sort()
             print(all_tokens)
-            for t in all_tokens:
-               ff.write(f"{t}\n")
+            for token in all_tokens:
+               new_file.write(f"{token}\n")
 
-        with io.open('2st_Task/lemmas/lemmas_' + str(f), 'w', encoding="utf-8") as ff:
+        with io.open('2st_Task/lemmas/lemmas_' + str(file), 'w', encoding="utf-8") as new_file:
             items = tokenized_map.items()
             items = sorted(items)
             for lemma, tokens in items:
-                ff.write(f"{lemma}\n")
+                new_file.write(f"{lemma}\n")
